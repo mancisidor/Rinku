feba215dd2db16e078388dbdea2ddc5b65496e8d
Jorge Manuel Mancisidor Clatza <sistemas.mancisidor@gmail.com>
2023-09-18 16:43:42 -0600

    se arreglan bugs

--- resources/views/empleados/index.blade.php
+++ resources/views/empleados/index.blade.php
@@ -23,6 +23,44 @@
     </table>
 </div>
 
+<!-- Modal para crear un nuevo empleado -->
+<div class="modal fade" id="crearEmpleadoModal" tabindex="-1" role="dialog" aria-labelledby="crearEmpleadoModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="crearEmpleadoModalLabel">Crear Nuevo Empleado</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <!-- Formulario para crear un nuevo empleado -->
+                <h2>Agregar Empleado</h2>
+                <form action="{{ route('agregar.empleado') }}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label for="nombre">Nombre:</label>
+                        <input type="text" class="form-control" id="nombre" name="nombre" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="tipo">Tipo:</label>
+                        <select class="form-control" id="tipo" name="tipo" required>
+                            <option value="chofer">Chofer</option>
+                            <option value="cargador">Cargador</option>
+                            <option value="auxiliar">Auxiliar</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="horas_trabajadas">Horas Trabajadas:</label>
+                        <input type="number" class="form-control" id="horas_trabajadas" name="horas_trabajadas" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Guardar</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Empresa Rinku</title>
     <!-- Agrega aquí tus enlaces a hojas de estilo CSS y scripts JavaScript -->
+    <!-- Estilos CSS de Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Scripts de JavaScript de Bootstrap (opcional) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+    <!-- Agrega las hojas de estilo CSS y el JavaScript de DataTables -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 </head>
 <body>
     <!-- Encabezado de tu aplicación, como un menú de navegación -->
